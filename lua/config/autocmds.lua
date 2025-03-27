d5166c6333d8bed65184ccc6c680cc0a2c587d91
ph4 <22990490+ph4@users.noreply.github.com>
2025-03-27 09:06:02 +0300

    close with q and esc

---
+++ lua/config/autocmds.lua
@@ -0,0 +1,27 @@
+vim.api.nvim_create_augroup('close_with_q', { clear = true })
+vim.api.nvim_create_autocmd('FileType', {
+  pattern = {
+    'qf',
+    'help',
+    'man',
+    'notify',
+    'lspinfo',
+    'spectre_panel',
+    'startuptime',
+    'tsplayground',
+    'PlenaryTestPopup',
+    'toggleterm',
+  },
+  callback = function(event)
+      vim.keymap.set('n', 'q', '<cmd>close<cr>', { buffer = event.buf, silent = true })
+  end,
+})
+
+
+vim.api.nvim_create_augroup('close_with_esc', { clear = true })
+vim.api.nvim_create_autocmd('FileType', {
+  pattern = {
+    'toggleterm',
+  },
+  callback = function(event) vim.keymap.set('n', '<esc>', '<cmd>close<cr>', { buffer = event.buf, silent = true }) end,
+})
