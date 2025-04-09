3df68df600caa793771e1ce262d47775b566e1a9
ph4 <22990490+ph4@users.noreply.github.com>
2025-04-09 23:16:14 +0300

    zk, mason to my registry

--- lua/plugins/ide/lsp.lua
+++ lua/plugins/ide/lsp.lua
@@ -1,9 +1,38 @@
 return {
   {
+    "zk-org/zk-nvim",
+    event = "VeryLazy",
+    config = function ()
+      require("zk").setup {
+        picker = 'telescope',
+        picker_options = {
+          --file picker
+          telescope = require('telescope.themes').get_dropdown {
+            results_title = "Results",
+            layout_strategy = 'horizontal',
+            layout_config = {
+              preview_width = 0.55,
+              height=0.85,
+              width = 0.85,
+              prompt_position = 'bottom',
+            },
+          },
+        },
+      }
+    end
+  },
+  {
     'neovim/nvim-lspconfig',
     event = 'VeryLazy',
     dependencies = {
-      { 'williamboman/mason.nvim', config = true},
+      {
+        'williamboman/mason.nvim',
+        opts = {
+          registries = {
+              "github:ph4/mason-registry",
+          },
+        }
+      },
       { 'williamboman/mason-lspconfig.nvim', config = true },
     },
     config = function()
