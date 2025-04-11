c2260cade32ca15834e244db620204cebcce276a
ph4 <22990490+ph4@users.noreply.github.com>
2025-04-11 20:18:22 +0300

    move render-markdonn in filetypes.lua

--- lua/plugins/ui_ux.lua
+++ lua/plugins/ui_ux.lua
@@ -264,11 +264,4 @@ return {
       end,
     },
   },
-  {
-    'MeanderingProgrammer/render-markdown.nvim',
-    dependencies = { 'nvim-treesitter/nvim-treesitter', 'nvim-tree/nvim-web-devicons' },
-    ---@module 'render-markdown'
-    ---@type render.md.UserConfig
-    opts = {},
-  }
 }
