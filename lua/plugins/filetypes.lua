092030671a08e780a0677b8fcbcbe9188fa7a815
ph4 <22990490+ph4@users.noreply.github.com>
2025-07-14 19:15:05 +0300

    conform

--- lua/plugins/filetypes.lua
+++ lua/plugins/filetypes.lua
@@ -6,4 +6,16 @@ return {
   --   ---@type render.md.UserConfig
   --   opts = {},
   -- },
+  {
+    'stevearc/conform.nvim',
+    opts = {
+      format_on_save = {
+        timeout_ms = 500,
+        lsp_format = 'fallback',
+      },
+      formatters_by_ft = {
+        json = { 'jq', '--indent', '2' },
+      },
+    }
+  }
 }
